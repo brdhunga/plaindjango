7940905c1e144f3a760ecaae35707d27323600bd
raj bro <brdhunga@gmail.com>
2020-03-27 09:08:47 -0400

    changes made for passkey

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Note(models.Model):
+    note_id = models.CharField(max_length=30, blank=True, default="", unique=True)
+    content = models.TextField(default="")
+
+    def __str__(self):
+        return self.note_id
--- notes/views.py
+++ notes/views.py
@@ -1,12 +1,46 @@
-from django.shortcuts import render
+import json
+from typing import Dict
+import os
+
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+
+from .models import Note
 
 
 def home(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("I am the home")
+    return HttpResponse("Maybe this will have all the notes")
 
 
+@csrf_exempt
 def render_single_note(request: HttpRequest, note_id: str) -> HttpResponse:
-    return render(request, 'single_note.html')
+
+    if request.method == "POST":
+        request_dict: Dict = json.loads(request.body)
+
+        # passKey, content
+        note_id = request_dict["noteId"]
+        pass_key = request_dict["passKey"]
+
+        if not note_id or not pass_key:
+            return HttpResponse("Note id or passkey cannot be empty", status=500)
+
+        if not os.environ["PASSKEY"] == pass_key:
+            return HttpResponse("passkey does not match", status=500)
+        
+        # Note object and boolean object
+        note, created = Note.objects.get_or_create(note_id=note_id)
+        note.content = request_dict["content"]
+        note.save()
+
+        return HttpResponse(note.content)
+
+    note, created = Note.objects.get_or_create(note_id=note_id)
+    if created:
+        content = ""
+    else:
+        content = note.content
+
+    return render(request, 'single_note.html', {'noteId': note_id, 'content': content})
 
